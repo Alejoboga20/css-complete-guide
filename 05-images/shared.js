8f196dd00635be6749a1ff70f527f84ba188ce7d
Alejo Boga <alejoboga20@gmail.com>
2023-01-16 18:22:27 -0500

    Handling modal with JS

--- 05-images/shared.js
+++ 05-images/shared.js
@@ -1,2 +1,19 @@
+const selectPlanButton = document.querySelectorAll('.plan button');
 const backdrop = document.querySelector('.backdrop');
-backdrop.style.display = 'block';
+const modal = document.querySelector('.modal');
+const closeModalButton = document.querySelector('.modal__action--negative');
+
+const closeModal = () => {
+	backdrop.style.display = 'none';
+	modal.style.display = 'none';
+};
+
+for (let i = 0; i < selectPlanButton.length; i++) {
+	selectPlanButton[i].addEventListener('click', () => {
+		backdrop.style.display = 'block';
+		modal.style.display = 'block';
+	});
+}
+
+closeModalButton.addEventListener('click', closeModal);
+backdrop.addEventListener('click', closeModal);
