8d63135aea469124e93e774b7a2970554ae3fefe
Alejo Boga <alejoboga20@gmail.com>
2023-01-25 11:10:25 -0500

    Transition to display

--- 08-grid/shared.js
+++ 08-grid/shared.js
@@ -10,14 +10,23 @@ const closeModal = () => {
 		modal.classList.remove('open');
 	}
 
-	backdrop.classList.remove('open');
+	backdrop.style.display = 'none';
 	mobileNav.classList.remove('open');
+
+	setTimeout(() => {
+		backdrop.style.display = 'none';
+		backdrop.classList.remove('open');
+	}, 200);
 };
 
 for (let i = 0; i < selectPlanButton.length; i++) {
 	selectPlanButton[i].addEventListener('click', () => {
 		modal.classList.add('open');
-		backdrop.classList.add('open');
+		backdrop.style.display = 'block';
+
+		setTimeout(() => {
+			backdrop.classList.add('open');
+		}, 10);
 	});
 }
 
@@ -29,5 +38,8 @@ backdrop.addEventListener('click', closeModal);
 
 toggleButton.addEventListener('click', () => {
 	mobileNav.classList.add('open');
-	backdrop.classList.add('open');
+
+	setTimeout(() => {
+		backdrop.classList.add('open');
+	}, 10);
 });
